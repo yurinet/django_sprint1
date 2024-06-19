aaab8173484847aad27d30fa7c4e8615122e6ae1
Yuriy Kuznetsov <yurinet29@yandex.ru>
2024-06-20 00:30:44 +0300

    pep8_error

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
-    path('admin/', admin.site.urls),]
+    path('admin/', admin.site.urls),
+]
